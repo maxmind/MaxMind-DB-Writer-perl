821f6003b4907df16d5eb8e89852afcaa68c9ce8
Dave Rolsky <autarch@urth.org>
2013-12-18 09:27:27 -0600

    Require next version of MaxMind::DB::Common

--- lib/MaxMind/DB/Writer/Tree.pm
+++ lib/MaxMind/DB/Writer/Tree.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use Math::Int128 0.06 qw( uint128 );
 use Math::Round qw( round );
-use MaxMind::DB::Common qw(
+use MaxMind::DB::Common 0.031003 qw(
     DATA_SECTION_SEPARATOR
     DATA_SECTION_SEPARATOR_SIZE
     METADATA_MARKER
