edc179468aa0a275c6650faf09a2b631f6f8c2bc
Dave Rolsky <autarch@urth.org>
2014-05-15 09:49:09 -0500

    Don't try to uncrustify uthash.h

--- dev-bin/uncrustify-all.sh
+++ dev-bin/uncrustify-all.sh
@@ -11,7 +11,7 @@ for dir in c; do
         $uncrustify $c_files;
     fi
     
-    h_files=`find $dir -maxdepth 1 -name '*.h' | grep -v ppport.h | grep -v int64.h | grep -v int128.h`
+    h_files=`find $dir -maxdepth 1 -name '*.h' | grep -v ppport.h | grep -v int64.h | grep -v int128.h | grep -v uthash.h`
     if [ "$h_files" != "" ]; then
         $uncrustify $h_files
         $uncrustify $h_files
