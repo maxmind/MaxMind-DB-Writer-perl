32d07c34f17c10d3bc0b1b13541d61c2318bed43
Dave Rolsky <autarch@urth.org>
2014-05-27 11:43:11 -0500

    Add some very basic docs for the MMDB writer code

--- lib/MaxMind/DB/Writer.pm
+++ lib/MaxMind/DB/Writer.pm
@@ -5,5 +5,44 @@ use warnings;
 
 1;
 
-# ABSTRACT: Create MaxMind DB files
+# ABSTRACT: Create MaxMind DB database files
 
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use MaxMind::DB::Writer::Tree;
+    use Net::Works::Network;
+
+    my $tree = MaxMind::DB::Writer::Tree->new(
+        ip_version    => 6,
+        record_size   => 24,
+        database_type => 'My-IP-Data',
+        languages     => ['en'],
+        description   => { en => 'My database of IP data' },
+    );
+
+    my $network
+        = Net::Works::Network->new_from_string( string => '8.23.0.0/16' );
+
+    $tree->insert_network(
+        $network,
+        {
+            color => 'blue',
+            dogs  => [ 'Fido', 'Ms. Pretty Paws' ],
+            size  => 42,
+        },
+    );
+
+    open my $fh, '>:raw', '/path/to/my-ip-data.mmdb';
+    $tree->write_tree($fh);
+
+=head1 DESCRIPTION
+
+This distribution contains the code necessary to write L<MaxMind DB database
+files|http://maxmind.github.io/MaxMind-DB/>. See L<MaxMind::DB::Writer::Tree>
+for API docs.
+
+=cut
--- lib/MaxMind/DB/Writer/Tree.pm
+++ lib/MaxMind/DB/Writer/Tree.pm
@@ -247,3 +247,235 @@ sub DEMOLISH {
 __PACKAGE__->meta()->make_immutable();
 
 1;
+
+# ABSTRACT: Tree representing a MaxMind DB database in memory - then write it to a file
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use MaxMind::DB::Writer::Tree;
+    use Net::Works::Network;
+
+    my $tree = MaxMind::DB::Writer::Tree->new(
+        ip_version    => 6,
+        record_size   => 24,
+        database_type => 'My-IP-Data',
+        languages     => ['en'],
+        description   => { en => 'My database of IP data' },
+    );
+
+    my $network
+        = Net::Works::Network->new_from_string( string => '8.23.0.0/16' );
+
+    $tree->insert_network(
+        $network,
+        {
+            color => 'blue',
+            dogs  => [ 'Fido', 'Ms. Pretty Paws' ],
+            size  => 42,
+        },
+    );
+
+    open my $fh, '>:raw', '/path/to/my-ip-data.mmdb';
+    $tree->write_tree($fh);
+
+=head1 DESCRIPTION
+
+This is the main class you'll use to write L<MaxMind DB database
+files|http://maxmind.github.io/MaxMind-DB/>. This class represents the
+database in memory. Once you've created the full tree you can write to a file.
+
+=head1 API
+
+This class provides the following methods:
+
+=head2 MaxMind::DB::Writer::Tree->new()
+
+This creates a new tree object. The constructor accepts the following
+parameters:
+
+=over 4
+
+=item * ip_version
+
+The IP version for the database. It must be 4 or 6.
+
+This parameter is required.
+
+=item * record_size
+
+This is the record size in I<bits>. This should be one of 24, 28, 32 (in
+theory any number divisible by 4 up to 128 will work but the available readers
+all expect 24-32).
+
+This parameter is required.
+
+=item * database_type
+
+This is a string containing the database type. This can be anything,
+really. MaxMind uses strings like "GeoIP2-City", "GeoIP2-Country", etc.
+
+This parameter is required.
+
+=item * languages
+
+This should be an array reference of languages used in the database, like
+"en", "zh-TW", etc. This is useful as metadata for database readers and end users.
+
+This parameter is optional.
+
+=item * description
+
+This is hashref where the keys are language names and the values are
+descriptions of the database in that language. For example, you might have
+something like:
+
+    {
+        en => 'My IP data',
+        fr => 'Mon Data de IP',
+    }
+
+
+This parameter is required.
+
+=item * map_key_type_callback
+
+This is a subroutine reference that is called in order to determine how to
+store each value in a map (hash) data structure. See L<DATA TYPES> below for
+more details.
+
+This parameter is optional.
+
+=item * merge_record_collisions
+
+By default, when an insert collides with a previous insert, the new data
+simply overwrites the old data where the two networks overlap.
+
+If this is set to true, then on a collision, the writer will merge the old
+data with the new data. This only works if both inserts use a hashref for the
+data payload.
+
+This parameter is optional. It defaults to false.
+
+=item * alias_ipv6_to_ipv4
+
+If this is true then the final database will map some IPv6 ranges to the IPv4
+range. These ranges are:
+
+=over 8
+
+=item * ::ffff:0:0/96
+
+This is the IPv4-mapped IPv6 range
+
+=item * 2001::/32
+
+This is the Teredo range. Note that lookups for Teredo ranges will find the
+Teredo server's IPv4 address, not the client's IPv4.
+
+=item * 2002::/16
+
+This is the 6to4 range
+
+=back
+
+This parameter is optional. It defaults to false.
+
+=back
+
+=head2 $tree->insert_network( $network, $data )
+
+This method expects to parameters. The first is a L<Net::Works::Network>
+object. The second can be any Perl data structure (except a coderef, glob, or
+filehandle).
+
+The C<$data> payload is encoded according to the L<MaxMind DB database format
+spec|http://maxmind.github.io/MaxMind-DB/>. The short overview is that
+anything that can be encoded in JSON can be stored in an MMDB file. It can
+also handle unsigned 64-bit 128-bit integers if they are passed as
+L<Math::UInt128> objects.
+
+=head2 $tree->write_tree($fh)
+
+Given a filehandle, this method writes the contents of the tree as a MaxMind
+DB database to that filehandle.
+
+=head1 DATA TYPES
+
+The MaxMind DB file format is strongly typed. Because Perl is not strongly
+typed, you will need to explicitly specify the types for each piece of
+data. Currently, this class assumes that your top-level data structure for an
+IP address will always be a map (hash). You can then provide a
+C<map_key_type_callback> subroutine that will be called as the data is
+serialized. This callback is given a key name and is expected to return that
+key's data type.
+
+Let's use the following structure as an example:
+
+    {
+        names => {
+            en => 'United States',
+            es => 'Estados Unidos',
+        },
+        population    => 319_000_000,
+        fizzle_factor => 65.7294,
+        states        => [ 'Alabama', 'Alaska', ... ],
+    }
+
+Given this data structure, our C<map_key_type_callback> might look something like this:
+
+    my %types = (
+        names         => 'map',
+        en            => 'utf8_string',
+        es            => 'utf8_string',
+        population    => 'uint32',
+        fizzle_factor => 'double',
+        states        => [ 'array', 'utf8_string' ],
+    );
+
+    sub {
+        my $key = shift;
+        return $type{$key};
+    }
+
+If the callback returns C<undef>, the serialization code will throw an
+error. Note that for an array we return a 2 element arrayref where the first
+element is C<'array'> and the second element is the type of content in the
+array.
+
+The valid types are:
+
+=over 4
+
+=item * utf8_string
+
+=item * uint16
+
+=item * uint32
+
+=item * uint64
+
+=item * uint128
+
+=item * int32
+
+=item * double
+
+64 bits of precision.
+
+=item * float
+
+32 bits of precision.
+
+=item * boolean
+
+=item * map
+
+=item * array
+
+=back
+
+=cut
