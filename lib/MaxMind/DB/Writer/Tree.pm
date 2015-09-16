19a1c9056f8b536a869e03109753379ac22de3e7
Olaf Alders <olaf@wundersolutions.com>
2015-09-16 17:53:13 -0400

    Fix SYNOPSIS errors in Tree.pm

--- lib/MaxMind/DB/Writer/Tree.pm
+++ lib/MaxMind/DB/Writer/Tree.pm
@@ -328,16 +328,23 @@ __END__
     use MaxMind::DB::Writer::Tree;
     use Net::Works::Network;
 
+    my %types = (
+        color => 'utf8_string',
+        dogs  => [ 'array', 'utf8_string' ],
+        size  => 'uint16',
+    );
+
     my $tree = MaxMind::DB::Writer::Tree->new(
-        ip_version    => 6,
-        record_size   => 24,
-        database_type => 'My-IP-Data',
-        languages     => ['en'],
-        description   => { en => 'My database of IP data' },
+        ip_version            => 6,
+        record_size           => 24,
+        database_type         => 'My-IP-Data',
+        languages             => ['en'],
+        description           => { en => 'My database of IP data' },
+        map_key_type_callback => sub { $types{ $_[0] } },
     );
 
     my $network
-        = Net::Works::Network->new_from_string( string => '8.23.0.0/16' );
+        = Net::Works::Network->new_from_string( string => '2001:db8::/48' );
 
     $tree->insert_network(
         $network,
