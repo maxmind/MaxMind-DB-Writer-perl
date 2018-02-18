4b125ff6f3322388f40b7d0a0a5e71f8d8509552
Ilya M <ilya77@gmail.com>
2018-02-18 03:49:05 +0200

    Uncrustified code

--- c/windows_mman.c
+++ c/windows_mman.c
@@ -36,8 +36,9 @@
 
 static int __map_mman_error(const DWORD err, const int deferr)
 {
-    if (err == 0)
+    if (err == 0) {
         return 0;
+    }
     //TODO: implement
     return err;
 }
@@ -46,18 +47,16 @@ static DWORD __map_mmap_prot_page(const int prot)
 {
     DWORD protect = 0;
 
-    if (prot == PROT_NONE)
+    if (prot == PROT_NONE) {
         return protect;
-
-    if ((prot & PROT_EXEC) != 0)
-    {
-        protect = ((prot & PROT_WRITE) != 0) ?
-                    PAGE_EXECUTE_READWRITE : PAGE_EXECUTE_READ;
     }
-    else
-    {
+
+    if ((prot & PROT_EXEC) != 0) {
         protect = ((prot & PROT_WRITE) != 0) ?
-                    PAGE_READWRITE : PAGE_READONLY;
+                  PAGE_EXECUTE_READWRITE : PAGE_EXECUTE_READ;
+    }else {
+        protect = ((prot & PROT_WRITE) != 0) ?
+                  PAGE_READWRITE : PAGE_READONLY;
     }
 
     return protect;
@@ -67,20 +66,24 @@ static DWORD __map_mmap_prot_file(const int prot)
 {
     DWORD desiredAccess = 0;
 
-    if (prot == PROT_NONE)
+    if (prot == PROT_NONE) {
         return desiredAccess;
+    }
 
-    if ((prot & PROT_READ) != 0)
+    if ((prot & PROT_READ) != 0) {
         desiredAccess |= FILE_MAP_READ;
-    if ((prot & PROT_WRITE) != 0)
+    }
+    if ((prot & PROT_WRITE) != 0) {
         desiredAccess |= FILE_MAP_WRITE;
-    if ((prot & PROT_EXEC) != 0)
+    }
+    if ((prot & PROT_EXEC) != 0) {
         desiredAccess |= FILE_MAP_EXECUTE;
+    }
 
     return desiredAccess;
 }
 
-void* mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off)
+void * mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off)
 {
     HANDLE fm, h;
 
@@ -92,18 +95,20 @@ void* mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off)
 #endif
 
     const DWORD dwFileOffsetLow = (sizeof(off_t) <= sizeof(DWORD)) ?
-                    (DWORD)off : (DWORD)(off & 0xFFFFFFFFL);
+                                  (DWORD)off : (DWORD)(off & 0xFFFFFFFFL);
     const DWORD dwFileOffsetHigh = (sizeof(off_t) <= sizeof(DWORD)) ?
-                    (DWORD)0 : (DWORD)((off >> 32) & 0xFFFFFFFFL);
+                                   (DWORD)0 : (DWORD)((off >> 32) &
+                                                      0xFFFFFFFFL);
     const DWORD protect = __map_mmap_prot_page(prot);
     const DWORD desiredAccess = __map_mmap_prot_file(prot);
 
     const off_t maxSize = off + (off_t)len;
 
     const DWORD dwMaxSizeLow = (sizeof(off_t) <= sizeof(DWORD)) ?
-                    (DWORD)maxSize : (DWORD)(maxSize & 0xFFFFFFFFL);
+                               (DWORD)maxSize : (DWORD)(maxSize & 0xFFFFFFFFL);
     const DWORD dwMaxSizeHigh = (sizeof(off_t) <= sizeof(DWORD)) ?
-                    (DWORD)0 : (DWORD)((maxSize >> 32) & 0xFFFFFFFFL);
+                                (DWORD)0 : (DWORD)((maxSize >> 32) &
+                                                   0xFFFFFFFFL);
 
 #ifdef _MSC_VER
 #pragma warning(pop)
@@ -115,35 +120,32 @@ void* mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off)
         /* Unsupported flag combinations */
         || (flags & MAP_FIXED) != 0
         /* Usupported protection combinations */
-        || prot == PROT_EXEC)
-    {
+        || prot == PROT_EXEC) {
         errno = EINVAL;
         return MAP_FAILED;
     }
 
     h = ((flags & MAP_ANONYMOUS) == 0) ?
-                    (HANDLE)_get_osfhandle(fildes) : INVALID_HANDLE_VALUE;
+        (HANDLE)_get_osfhandle(fildes) : INVALID_HANDLE_VALUE;
 
-    if ((flags & MAP_ANONYMOUS) == 0 && h == INVALID_HANDLE_VALUE)
-    {
+    if ((flags & MAP_ANONYMOUS) == 0 && h == INVALID_HANDLE_VALUE) {
         errno = EBADF;
         return MAP_FAILED;
     }
 
     fm = CreateFileMapping(h, NULL, protect, dwMaxSizeHigh, dwMaxSizeLow, NULL);
 
-    if (fm == NULL)
-    {
+    if (fm == NULL) {
         errno = __map_mman_error(GetLastError(), EPERM);
         return MAP_FAILED;
     }
 
-    map = MapViewOfFile(fm, desiredAccess, dwFileOffsetHigh, dwFileOffsetLow, len);
+    map = MapViewOfFile(fm, desiredAccess, dwFileOffsetHigh, dwFileOffsetLow,
+                        len);
 
     CloseHandle(fm);
 
-    if (map == NULL)
-    {
+    if (map == NULL) {
         errno = __map_mman_error(GetLastError(), EPERM);
         return MAP_FAILED;
     }
@@ -153,10 +155,11 @@ void* mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off)
 
 int munmap(void *addr, size_t len)
 {
-    if (UnmapViewOfFile(addr))
+    if (UnmapViewOfFile(addr)) {
         return 0;
+    }
 
-    errno =  __map_mman_error(GetLastError(), EPERM);
+    errno = __map_mman_error(GetLastError(), EPERM);
 
     return -1;
 }
@@ -166,40 +169,44 @@ int mprotect(void *addr, size_t len, int prot)
     DWORD newProtect = __map_mmap_prot_page(prot);
     DWORD oldProtect = 0;
 
-    if (VirtualProtect(addr, len, newProtect, &oldProtect))
+    if (VirtualProtect(addr, len, newProtect, &oldProtect)) {
         return 0;
+    }
 
-    errno =  __map_mman_error(GetLastError(), EPERM);
+    errno = __map_mman_error(GetLastError(), EPERM);
 
     return -1;
 }
 
 int msync(void *addr, size_t len, int flags)
 {
-    if (FlushViewOfFile(addr, len))
+    if (FlushViewOfFile(addr, len)) {
         return 0;
+    }
 
-    errno =  __map_mman_error(GetLastError(), EPERM);
+    errno = __map_mman_error(GetLastError(), EPERM);
 
     return -1;
 }
 
 int mlock(const void *addr, size_t len)
 {
-    if (VirtualLock((LPVOID)addr, len))
+    if (VirtualLock((LPVOID)addr, len)) {
         return 0;
+    }
 
-    errno =  __map_mman_error(GetLastError(), EPERM);
+    errno = __map_mman_error(GetLastError(), EPERM);
 
     return -1;
 }
 
 int munlock(const void *addr, size_t len)
 {
-    if (VirtualUnlock((LPVOID)addr, len))
+    if (VirtualUnlock((LPVOID)addr, len)) {
         return 0;
+    }
 
-    errno =  __map_mman_error(GetLastError(), EPERM);
+    errno = __map_mman_error(GetLastError(), EPERM);
 
     return -1;
 }
--- c/windows_mman.h
+++ c/windows_mman.h
@@ -58,7 +58,8 @@
 #define MS_SYNC         2
 #define MS_INVALIDATE   4
 
-void*   mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off);
+void *   mmap(void *addr, size_t len, int prot, int flags, int fildes,
+              off_t off);
 int     munmap(void *addr, size_t len);
 int     mprotect(void *addr, size_t len, int prot);
 int     msync(void *addr, size_t len, int flags);
